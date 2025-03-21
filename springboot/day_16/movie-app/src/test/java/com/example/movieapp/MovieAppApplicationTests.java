27b613bee0b0e73e60674dc9fa21089bc265a5b6
buihien0109 <buihien01091997@gmail.com>
2025-03-21 20:07:13 +0700

    update

--- springboot/day_16/movie-app/src/test/java/com/example/movieapp/MovieAppApplicationTests.java
+++ springboot/day_16/movie-app/src/test/java/com/example/movieapp/MovieAppApplicationTests.java
@@ -1,8 +1,8 @@
 package com.example.movieapp;
 
-import com.example.movieapp.entity.Movie;
+import com.example.movieapp.entity.*;
 import com.example.movieapp.model.enums.MovieType;
-import com.example.movieapp.repository.MovieRepository;
+import com.example.movieapp.repository.*;
 import com.github.javafaker.Faker;
 import com.github.slugify.Slugify;
 import org.junit.jupiter.api.Test;
@@ -21,7 +21,113 @@ import java.util.Random;
 class MovieAppApplicationTests {
 
     @Autowired
+    private ActorRepository actorRepository;
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Autowired
+    private DirectorRepository directorRepository;
+
+    @Autowired
+    private EpisodeRepository episodeRepository;
+
+    @Autowired
+    private FavoriteRepository favoriteRepository;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Autowired
+    private HistoryRepository historyRepository;
+
+    @Autowired
+    private MediaRepository mediaRepository;
+
+    @Autowired
     private MovieRepository movieRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void save_countries() {
+        Faker faker = new Faker();
+        Slugify slugify = Slugify.builder().build();
+        for (int i = 0; i < 10; i++) {
+            String name = faker.country().name();
+            Country country = Country.builder()
+                    .name(name)
+                    .slug(slugify.slugify(name))
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .build();
+            countryRepository.save(country);
+        }
+    }
+
+    @Test
+    void save_genres() {
+        Faker faker = new Faker();
+        Slugify slugify = Slugify.builder().build();
+        for (int i = 0; i < 10; i++) {
+            String name = faker.leagueOfLegends().champion();
+            Genre genre = Genre.builder()
+                    .name(name)
+                    .slug(slugify.slugify(name))
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .build();
+            genreRepository.save(genre);
+        }
+    }
+
+    @Test
+    void save_actors() {
+        Faker faker = new Faker();
+        Slugify slugify = Slugify.builder().build();
+        for (int i = 0; i < 100; i++) {
+            String name = faker.name().fullName();
+            Actor actor = Actor.builder()
+                    .name(name)
+                    .slug(slugify.slugify(name))
+                    .avatar("https://placehold.co/600x400?text=" + name.substring(0, 1).toUpperCase())
+                    .bio(faker.lorem().paragraph())
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .build();
+            actorRepository.save(actor);
+        }
+    }
+
+    @Test
+    void save_directors() {
+        Faker faker = new Faker();
+        Slugify slugify = Slugify.builder().build();
+        for (int i = 0; i < 30; i++) {
+            String name = faker.name().fullName();
+            Director director = Director.builder()
+                    .name(name)
+                    .slug(slugify.slugify(name))
+                    .avatar("https://placehold.co/600x400?text=" + name.substring(0, 1).toUpperCase())
+                    .bio(faker.lorem().paragraph())
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .build();
+            directorRepository.save(director);
+        }
+    }
+
+    @Test
+    void save_users() {
+
+    }
 
     @Test
     void save_movies() {
