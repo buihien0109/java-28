008946f0fcae5be7326cb096e0de32a20be3b73e
buihien0109 <buihien01091997@gmail.com>
2025-03-21 20:54:45 +0700

    update

--- springboot/day_16/movie-app/src/test/java/com/example/movieapp/MovieAppApplicationTests.java
+++ springboot/day_16/movie-app/src/test/java/com/example/movieapp/MovieAppApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.movieapp;
 
 import com.example.movieapp.entity.*;
 import com.example.movieapp.model.enums.MovieType;
+import com.example.movieapp.model.enums.UserRole;
 import com.example.movieapp.repository.*;
 import com.github.javafaker.Faker;
 import com.github.slugify.Slugify;
@@ -14,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -126,7 +128,50 @@ class MovieAppApplicationTests {
 
     @Test
     void save_users() {
+        Faker faker = new Faker();
+        for (int i = 0; i < 50; i++) {
+            String displayName = faker.name().fullName();
+            User user = User.builder()
+                    .username(faker.name().username())
+                    .displayName(displayName)
+                    .email(faker.internet().emailAddress())
+                    .avatar("https://placehold.co/600x400?text=" + displayName.substring(0, 1).toUpperCase())
+                    .phone(faker.phoneNumber().cellPhone())
+                    .password("123")
+                    .role(i < 2 ? UserRole.ADMIN : UserRole.USER)
+                    .isEnabled(true)
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .build();
+            userRepository.save(user);
+        }
+    }
 
+    @Test
+    void save_posts() {
+        Faker faker = new Faker();
+        Slugify slugify = Slugify.builder().build();
+
+        List<User> users = userRepository.findByRole(UserRole.ADMIN);
+
+        for (int i = 0; i < 50; i++) {
+            String title = faker.book().title();
+            Boolean status = faker.bool().bool();
+            User rdUser = users.get(faker.number().numberBetween(0, users.size()));
+            Post post = Post.builder()
+                    .title(title)
+                    .slug(slugify.slugify(title))
+                    .content(faker.lorem().paragraph(50))
+                    .description(faker.lorem().paragraph())
+                    .thumbnail("https://placehold.co/600x400?text=" + title.substring(0, 1).toUpperCase())
+                    .status(status)
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .publishedAt(status ? LocalDateTime.now() : null)
+                    .user(rdUser)
+                    .build();
+            postRepository.save(post);
+        }
     }
 
     @Test
@@ -135,7 +180,45 @@ class MovieAppApplicationTests {
         Slugify slugify = Slugify.builder().build();
         Random rd = new Random();
 
+        List<Country> countries = countryRepository.findAll();
+        List<Genre> genres = genreRepository.findAll();
+        List<Actor> actors = actorRepository.findAll();
+        List<Director> directors = directorRepository.findAll();
+
         for (int i = 0; i < 150; i++) {
+            // Random a country
+            Country rdCountry = countries.get(rd.nextInt(countries.size()));
+
+            // Random list genres
+            List<Genre> rdGenres = new ArrayList<>();
+            // Random 2 -> 4 genres
+            for (int j = 0; j < rd.nextInt(3) + 2; j++) {
+                Genre rdGenre = genres.get(rd.nextInt(genres.size()));
+                if (!rdGenres.contains(rdGenre)) {
+                    rdGenres.add(rdGenre);
+                }
+            }
+
+            // Random list actors
+            List<Actor> rdActors = new ArrayList<>();
+            // Random 4 -> 7 actors
+            for (int j = 0; j < rd.nextInt(4) + 4; j++) {
+                Actor rdActor = actors.get(rd.nextInt(actors.size()));
+                if (!rdActors.contains(rdActor)) {
+                    rdActors.add(rdActor);
+                }
+            }
+
+            // Random list directors
+            List<Director> rdDirectors = new ArrayList<>();
+            // Random 1 -> 2 directors
+            for (int j = 0; j < rd.nextInt(2) + 1; j++) {
+                Director rdDirector = directors.get(rd.nextInt(directors.size()));
+                if (!rdDirectors.contains(rdDirector)) {
+                    rdDirectors.add(rdDirector);
+                }
+            }
+
             // Tao entity
             String name = faker.funnyName().name();
             String thumbnail = "https://placehold.co/600x400?text=" + name.substring(0, 1).toUpperCase();
@@ -157,10 +240,78 @@ class MovieAppApplicationTests {
                     .createdAt(LocalDateTime.now())
                     .updatedAt(LocalDateTime.now())
                     .publishedAt(status ? LocalDateTime.now() : null)
+                    .country(rdCountry)
+                    .genres(rdGenres)
+                    .actors(rdActors)
+                    .directors(rdDirectors)
                     .build();
 
             // Luu vao DB
             movieRepository.save(movie);
+        }
+    }
+
+    @Test
+    void save_episodes() {
+        Faker faker = new Faker();
+        List<Movie> movies = movieRepository.findAll();
+
+        for (Movie movie : movies) {
+            if (movie.getType().equals(MovieType.PHIM_BO)) {
+                // random 5 -> 10 episodes
+                for (int i = 0; i < faker.number().numberBetween(5, 11); i++) {
+                    Episode episode = Episode.builder()
+                            .name("Tập " + (i + 1))
+                            .duration(faker.number().numberBetween(40, 60))
+                            .displayOrder(i + 1)
+                            .videoUrl("https://www.youtube.com/embed/W_0AMP9yO1w?si=JcCeGorHalCHKCPl")
+                            .status(true)
+                            .createdAt(LocalDateTime.now())
+                            .updatedAt(LocalDateTime.now())
+                            .publishedAt(LocalDateTime.now())
+                            .movie(movie)
+                            .build();
+                    episodeRepository.save(episode);
+                }
+            } else {
+                Episode episode = Episode.builder()
+                        .name("Tập full")
+                        .duration(faker.number().numberBetween(90, 120))
+                        .displayOrder(1)
+                        .videoUrl("https://www.youtube.com/embed/W_0AMP9yO1w?si=JcCeGorHalCHKCPl")
+                        .status(true)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .publishedAt(LocalDateTime.now())
+                        .movie(movie)
+                        .build();
+                episodeRepository.save(episode);
+            }
+        }
+    }
+
+    @Test
+    void save_reviews() {
+        Faker faker = new Faker();
+        Random rd = new Random();
+
+        List<User> users = userRepository.findAll();
+        List<Movie> movies = movieRepository.findAll();
+
+        for (Movie movie : movies) {
+            // random 10 -> 20 reviews
+            for (int i = 0; i < rd.nextInt(11) + 10; i++) {
+                User rdUser = users.get(rd.nextInt(users.size()));
+                Review review = Review.builder()
+                        .content(faker.lorem().paragraph())
+                        .rating(rd.nextInt(6) + 5)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .user(rdUser)
+                        .movie(movie)
+                        .build();
+                reviewRepository.save(review);
+            }
         }
     }
 
