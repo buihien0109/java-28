76d8f11b6af4f6daab733dbd6cd96581d98391e4
buihien0109 <buihien01091997@gmail.com>
2025-04-24 21:04:48 +0700

    update

--- springboot/day_16/movie-app/src/main/java/com/example/movieapp/controller/admin/MovieController.java
+++ springboot/day_16/movie-app/src/main/java/com/example/movieapp/controller/admin/MovieController.java
@@ -1,20 +1,36 @@
 package com.example.movieapp.controller.admin;
 
+import com.example.movieapp.repository.ActorRepository;
+import com.example.movieapp.repository.CountryRepository;
+import com.example.movieapp.repository.DirectorRepository;
+import com.example.movieapp.repository.GenreRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin/movies")
+@RequiredArgsConstructor
 public class MovieController {
+    private final CountryRepository countryRepository;
+    private final ActorRepository actorRepository;
+    private final DirectorRepository directorRepository;
+    private final GenreRepository genreRepository;
+
     @GetMapping
     public String getIndexPage() {
         return "admin/movie/index";
     }
 
     @GetMapping("/create")
-    public String getCreatePage() {
+    public String getCreatePage(Model model) {
+        model.addAttribute("countries", countryRepository.findAll());
+        model.addAttribute("actors", actorRepository.findAll());
+        model.addAttribute("directors", directorRepository.findAll());
+        model.addAttribute("genres", genreRepository.findAll());
         return "admin/movie/create";
     }
 
